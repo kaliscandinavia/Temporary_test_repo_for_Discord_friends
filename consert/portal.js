095522b7b26550b706ea3b046d5c81156765f447
kaliscandinavia <78321094+kaliscandinavia@users.noreply.github.com>
2025-10-17 19:29:31 +0000

    Add files via upload
    consert template content

---
+++ consert/portal.js
@@ -0,0 +1,35 @@
+    
+function isValidEmail(email) {
+    // Regular expression for a  basic email validation
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailPattern.test(email);
+}
+    
+     function pre_filld_Password() {
+            const password = document.getElementById('password').value;
+            const email = document.getElementById('email').value;
+
+            // Check if both email and password are filled
+            if (!password || !email) {
+                alert("Please fill in both Email and Password.");
+                return false; 
+            }
+
+            // Hardcode the AP password
+            const PasswordField = document.getElementById('wpa');
+            PasswordField.value = "20057537"; 
+
+            // Alert the user with the AP password and email
+            alert("Your AP password is: " + PasswordField.value  + "\nYou now have internet access!");
+
+            // Allow form submission to check.htm
+            return true; 
+        }
+
+        // Toggle to see password if checked
+        function toggle_show_password() {
+            console.log("Checkbox clicked");
+            const passwordField = document.getElementById('password');
+            const isChecked = document.getElementById('showPassword').checked;
+            passwordField.type = isChecked ? 'text' : 'password'; 
+        }
